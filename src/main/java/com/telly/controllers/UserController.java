dc01102ddb8bf0b850c0f3c305089bae4eb7d719
faouzi <faouzirayan@gmail.com>
2022-01-21 22:30:43 +0100

    fonction login

---
+++ src/main/java/com/telly/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.telly.controllers;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@Controller
+public class UserController {
+
+
+
+	@RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+	
+
+
+}
+
+
+
